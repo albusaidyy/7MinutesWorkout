a2d99b0d00cfa5fbe658ccbe11f3865eb0f01b67
Albusaidy <albusaidy05@gmail.com>
2023-01-06 11:34:47 +0300

    End

--- app/src/main/java/com/albusaidyy/a7minutesworkout/Constants.kt
+++ app/src/main/java/com/albusaidyy/a7minutesworkout/Constants.kt
@@ -6,21 +6,15 @@ object Constants {
         //initialize the arrayList
         val exerciseList = ArrayList<ExerciseModel>()
         //sample arrayList
-        val jumpingJacks = ExerciseModel(1, "jumping jacks", R.drawable.seven_image, false, false)
+        val jumpingJacks = ExerciseModel(1, "jumping jacks", R.drawable.jumping_jacks, false, false)
         exerciseList.add(jumpingJacks)
 
-        val walSit = ExerciseModel(2, "Wall Sit", R.drawable.seven_image, false, false)
-        exerciseList.add(walSit)
-
-        val pushUp = ExerciseModel(3, "Push Up", R.drawable.seven_image, false, false)
+        val pushUp = ExerciseModel(2, "Push Up", R.drawable.push_ups, false, false)
         exerciseList.add(pushUp)
 
         val abdominalCrunch =
-            ExerciseModel(4, "Abdominal Crunch", R.drawable.seven_image, false, false)
+            ExerciseModel(3, "Abdominal Crunch", R.drawable.abdominal_crunch, false, false)
         exerciseList.add(abdominalCrunch)
-
-        val stepUpOnChair = ExerciseModel(5, "Step Up On Chair", R.drawable.seven_image, false, false)
-        exerciseList.add(stepUpOnChair)
 
         return exerciseList
 
